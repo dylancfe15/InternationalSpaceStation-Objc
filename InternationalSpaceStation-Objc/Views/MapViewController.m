322610e8b75f16b67c167ed4b4f99958b0f78c8f
Dylan Chen <dylancfe15@gmail.com>
2022-09-25 14:53:39 -0400

    Update MapViewController.m

--- InternationalSpaceStation-Objc/Views/MapViewController.m
+++ InternationalSpaceStation-Objc/Views/MapViewController.m
@@ -7,10 +7,12 @@
 
 #import "MapViewController.h"
 #import "MapViewModel.h"
+#import <UIKit/UIKit.h>
 
 @implementation MapViewController
 
 MapViewModel *viewModel;
+UIButton *focusButton;
 
 - (void)viewDidLoad {
     [super viewDidLoad];
@@ -19,15 +21,34 @@ MapViewModel *viewModel;
 }
 
 - (void)configureUI {
-    _mapView = [[MapView alloc] init];
-
     [self.view addSubview: _mapView];
+    [self.view addSubview: focusButton];
+
+    // MapView
+    _mapView = [[MapView alloc] init];
 
     [NSLayoutConstraint activateConstraints: @[
         [_mapView.topAnchor constraintEqualToAnchor: self.view.topAnchor],
         [_mapView.leadingAnchor constraintEqualToAnchor: self.view.leadingAnchor],
         [_mapView.bottomAnchor constraintEqualToAnchor: self.view.bottomAnchor],
         [_mapView.trailingAnchor constraintEqualToAnchor: self.view.trailingAnchor]
+    ]];
+
+    // FocusButton
+    focusButton = [UIButton buttonWithType:UIButtonTypeCustom];
+    focusButton.translatesAutoresizingMaskIntoConstraints = false;
+    [focusButton setImage:[UIImage imageNamed:@"focus"] forState:(UIControlStateNormal)];
+    focusButton.backgroundColor = [UIColor whiteColor];
+    focusButton.layer.shadowRadius = 5;
+    focusButton.layer.shadowColor = [[UIColor grayColor] CGColor];
+    focusButton.layer.shadowRadius = 10;
+    [focusButton addTarget:self action:@selector(didFocusButtonOnTap:) forControlEvents:UIControlEventTouchUpInside];
+
+    [NSLayoutConstraint activateConstraints: @[
+        [focusButton.widthAnchor constraintEqualToConstant:44],
+        [focusButton.heightAnchor constraintEqualToConstant:44],
+        [focusButton.bottomAnchor constraintEqualToAnchor:self.view.bottomAnchor constant:-75],
+        [focusButton.trailingAnchor constraintEqualToAnchor:self.view.trailingAnchor constant:-16]
     ]];
 
     viewModel = [MapViewModel alloc];
@@ -37,6 +58,12 @@ MapViewModel *viewModel;
     } onFailure:^(NSError *error) {
         // TODO: Handle error
     }];
+}
+
+- (void) didFocusButtonOnTap:(id)sender {
+    _mapView.shouldFocusOnISS = !_mapView.shouldFocusOnISS;
+
+    focusButton.alpha = _mapView.shouldFocusOnISS ? 1 : 0.5;
 }
 
 @end
